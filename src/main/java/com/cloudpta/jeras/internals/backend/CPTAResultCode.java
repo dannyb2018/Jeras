c46a2b377bee99958625c2833fe0324ad470e5b7
dannyb2018 <dannyb@cloudpta.com>
2019-08-07 18:04:10 +0900

    Jeras result codes that should be the same across all backends

---
+++ src/main/java/com/cloudpta/jeras/internals/backend/CPTAResultCode.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cloudpta.jeras.internals.backend;
+
+/**
+ *
+ * @author PC
+ */
+public enum CPTAResultCode
+{
+    OK,
+    CANCELLED,
+    UNKNOWN,
+    INVALID_ARGUMENT,
+    DEADLINE_EXCEEDED,
+    NOT_FOUND,
+    ALREADY_EXISTS,
+    PERMISSION_DENIED,
+    UNAUTHENTICATED,
+    RESOURCE_EXHAUSTED,
+    FAILED_PRECONDITION,
+    ABORTED,
+    OUT_OF_RANGE,
+    UNIMPLEMENTED,
+    INTERNAL,
+    UNAVAILABLE,
+    DATA_LOSS 
+}
